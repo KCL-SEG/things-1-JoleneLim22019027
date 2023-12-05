2163e03205b4c3bcf4d6a2a2fec3e1c60ab3dd4b
Jolene Lim <146197714+JoleneLim22019027@users.noreply.github.com>
2023-12-05 00:58:36 +0000

    exercise 3

--- things/models.py
+++ things/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class User(AbstractUser):
-   name = models.TextField(max_length=50)
-   description = models.TextField(max_length=200)
-   quantity = models.IntegerField()
+   name = models.TextField(max_length=30, unique=True, blank=False)
+   description = models.TextField(max_length=120, unique=False, blank=True)
+   quantity = models.IntegerField(
+      validators=[
+         MinValueValidator(0, message="Value must be greater than or equal to 0"),
+         MaxValueValidator(100, message="Value must be lesser than or equal to 100")
+      ]
+   )
